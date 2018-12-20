a1fa1342d775e1c0d11d72449d7b68206b86fd32
takuya8820 <s206226@center.wakayama-u.ac.jp>
2018-12-20 19:14:23 +0900

    cwvwv

--- jikken3.py
+++ jikken3.py
@@ -59,7 +59,7 @@ testFakeRatios = [0.1, 0.2, 0.3, 0.4, 0.5]
 threFake = 0.5
 
 # Rの二乗誤差の閾値
-threSquaredLoss = 2000
+threSquaredLoss = 4000
 
 # ファイル名のpostFix
 postFix = "{}_{}".format(targetChar, trialNo)
@@ -443,8 +443,8 @@ for ite in range(15000):
 	# 学習
     if trainMode == 0:
         
-        _, lossR_value, lossRAll_value, lossD_value, decoderR_train_value, encoderR_train_value, predictFake_train_value, predictTrue_train_value, decoderR_fake_train_value = sess.run(
-                [trainerRAll, lossR, lossRAll, lossD, decoderR_train, encoderR_train, predictFake_train, predictTrue_train, decoderR_fake_train],
+        _, lossR_value, lossRAll_value, lossD_value, decoderR_train_value, encoderR_train_value, predictFake_train_value, predictTrue_train_values = sess.run(
+                [trainerRAll, lossR, lossRAll, lossD, decoderR_train, encoderR_train, predictFake_train, predictTrue_train],
                 feed_dict={xTrue: batch_x, xFake: batch_x_fake})
         if lossR_value < threSquaredLoss:
             trainMode = 1
