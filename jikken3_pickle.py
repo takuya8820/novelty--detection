04d001df275e9ad91217f9e0d5d3d2100f680d3f
takuya8820 <s206226@center.wakayama-u.ac.jp>
2018-12-30 00:11:19 +0900

    fheh

--- jikken3_pickle.py
+++ jikken3_pickle.py
@@ -20,11 +20,12 @@ if len(sys.argv) > 1:
         trialNo = int(sys.argv[2])
         # noiseSigma
         if len(sys.argv) > 3:
-            noiseSigma = int(sys.argv[3])
+            noisez = int(sys.argv[3])
             #late
             if len(sys.argv) > 4:
                     late = int(sys.argv[4])
             
+noiseSigma = 51
 threSquaredLoss = 200
 logPath = 'logs'
 jikkenPath = 'jikken'
@@ -33,14 +34,14 @@ jikkenPath3 = 'jikken3'
 postFix = "{}_{}".format(targetChar, trialNo)
 
 #path1 = os.path.join(logPath,"noiseSigma_{}".format(noiseSigma))
-path1 = os.path.join(jikkenPath3,"noiseSigma{}".format(noiseSigma))
+path1 = os.path.join(jikkenPath3,"noiseZ_{}".format(noisez))
 path = os.path.join(path1,"log{}.pickle".format(postFix))
 with open(path, "rb") as fp:
     batch = pickle.load(fp)
     batch_x_fake = pickle.load(fp)
     encoderR_train_value = pickle.load(fp)
     decoderR_train_value = pickle.load(fp)
-    encoderR_fake_train_value = pickle.load(fp)
+    #encoderR_fake_train_value = pickle.load(fp)
     decoderR_fake_train_value = pickle.load(fp)
     predictFake_train_value = pickle.load(fp)
     predictTrue_train_value = pickle.load(fp)
@@ -60,8 +61,8 @@ with open(path, "rb") as fp:
     lossD_values = pickle.load(fp)
     params = pickle.load(fp)
     
-print(precisionDRXs)
-
+print(precisionDXs[late][14])
+print(precisionDRXs[late][14])
 '''    
 print(encoderR_train_value)
 #zと何を比較して散布図に乗せればよい？
