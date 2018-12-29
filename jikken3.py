cb17974bd007472526607a0317a62873f2caa160
takuya8820 <s206226@center.wakayama-u.ac.jp>
2018-12-30 00:17:08 +0900

    提案法２

--- jikken3.py
+++ jikken3.py
@@ -476,7 +476,7 @@ for ite in range(20000):
     lossD_values.append(lossD_value)
     
     if ite% 100 == 0:
-        print("#%d %d(%d)(%d), lossR=%f, lossRAll=%f, lossD=%f" % (ite, targetChar, trialNo, noiseSigma, lossR_value, lossRAll_value, lossD_value))
+        print("#%d %d(%d)(%d), lossR=%f, lossRAll=%f, lossD=%f" % (ite, targetChar, trialNo, noisez, lossR_value, lossRAll_value, lossD_value))
 	#--------------
 
 	#--------------
