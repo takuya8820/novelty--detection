996c13208a128102a01c030ca1948faf2ede6fb6
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-01-12 14:40:35 +0900

    pvnqip

--- jikken3_oresen.py
+++ jikken3_oresen.py
@@ -24,12 +24,14 @@ if len(sys.argv) > 1:
 jikkenPath3 = 'jikken3'
 
 data = []
-mx = []
-x = []
-y = []
+mx1 = []
+mx2 = []
+y1 = []
+y2 = []
 for late in range(4):
     for targetChar in range(10):
-        data = []
+        data1 = []
+        data2 = []
         for trialNo in range(1,4):
             postFix = "{}_{}".format(targetChar, trialNo)
             path1 = os.path.join(jikkenPath3,"noise{}_{}".format(noisez,noiseSigma))
@@ -60,15 +62,40 @@ for late in range(4):
                 params = pickle.load(fp)
                 
                 #dataは3回実験した結果を格納
-            data.append(precisionDXs[late][14])
+            data1.append(precisionDXs[late][14])
+            data2.append(precisionDRXs[late][14])
                 #mxは各カテゴリの最大値を格納
-        mx.append(max(data))
+        mx1.append(max(data1))
+        mx2.append(max(data2))
     
-    s=sum(mx)
-    n=len(mx)
+    s1=sum(mx1)
+    n1=len(mx1)
+    s2=sum(mx2)
+    n2=len(mx2)
     #meanは任意のnoiseSigmaとnoiseZのときのF値
-    mean=s/n
-    y.append(mean)
+    mean1=s1/n1
+    mean2=s2/n2
+    y1.append(mean1)
+    y2.append(mean2)
     pdb.set_trace()
+    
+sns.set()
+sns.set_style('white')
+sns.set_palette('Set1')
+sns.set_context("paper")
+    
+x = np.array([10,20,30,40,50])
 
-plt.plot(x,y)    
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.plot(x, y1, label='D(X)')
+ax.plot(x, y2, label='D(R(X))')
+
+ax.legend()
+ax.set_xlabel("Percentage of outliers(%)")
+ax.set_ylabel("F1-Score")
+ax.set_xlim(10, 50)
+ax.set_ylim(0, 100)
+
+plt.show()
