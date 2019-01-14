ee2218524bc5a4252cb9bfedd6f6e411747d1bdc
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-01-14 12:14:48 +0900

    incq

--- jikken2.py
+++ jikken2.py
@@ -404,7 +404,7 @@ for ite in range(16000):
 	lossD_values.append(lossD_value)
 	
 	if ite%100 == 0:
-		print("#%d %d(%d), lossR=%f, lossRAll=%f, lossD=%f" % (ite, targetChar, trialNo, lossR_value, lossRAll_value, lossD_value))
+		print("#%d %d(%d)(%d), lossR=%f, lossRAll=%f, lossD=%f" % (ite, targetChar, trialNo, threSquaredLoss, lossR_value, lossRAll_value, lossD_value))
 	#--------------
 
 	#--------------
