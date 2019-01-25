bb270811ca4dac531451b298387ddf76e36f7642
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-01-25 22:21:25 +0900

    on

--- jikken2_pickle.py
+++ jikken2_pickle.py
@@ -33,7 +33,7 @@ for targetChar in range(10):
     data = []
     for trialNo in range(1,4):
         postFix = "{}_{}".format(targetChar, trialNo)
-        path1 = os.path.join(jikkenPath2,"noise{}_{}".format(noiseSigma,threSquaredLoss))
+        path1 = os.path.join(jikkenPath2,"noise{}_{}".format(threSquaredLoss,noiseSigma))
         path = os.path.join(path1,"log{}.pickle".format(postFix))
         with open(path, "rb") as fp:
             batch = pickle.load(fp)
