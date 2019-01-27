1cd9b64d01e1b448e551e4f7f7f6fa62ba10de78
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-01-27 16:02:17 +0900

    r

--- jikken_pickle.py
+++ jikken_pickle.py
@@ -60,8 +60,7 @@ for targetChar in range(10):
             lossD_values = pickle.load(fp)
             params = pickle.load(fp)
             
-            pdb.set_trace()
-            if lossD_values==-11.512916:
+            if lossD_values[14999]==-11.512916:
                 print("失敗")
             else:
                 print(precisionDRXs[late][14])
