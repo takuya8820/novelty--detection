d02e667d74a07614040f3d34f3128ccb4011881c
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-02-11 10:54:51 +0900

    F値比較

--- jikken3_oresen.py
+++ jikken3_oresen.py
@@ -232,8 +232,8 @@ plt.rc('font', **font)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-ax.plot(x, y1, label='DODANFS D(X)', marker="o")
-ax.plot(x, y2, label='DODANFS D(R(X))', marker="o")
+ax.plot(x, y1, label='NAFS D(X)', marker="o")
+ax.plot(x, y2, label='NAFS D(R(X))', marker="o")
 ax.plot(x, y3, label='ALOCC D(X)')
 ax.plot(x, y4, label='ALOCC D(R(X))')
 
