f50a72617b882c2c91aa5c092f16c4c8c2f111b0
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-02-12 10:41:43 +0900

    可視化

--- bunseki.py
+++ bunseki.py
@@ -91,18 +91,19 @@ y2=encoderR_train_value[:,1]
 fig, ax = plt.subplots()
 plt.gray()
 
-'''
+
 for i in range(number):
     imscatter(encoderR_fake_train_value[i,0], encoderR_fake_train_value[i,1], decoderR_fake_train_value[i,:,:,0], ax=ax, zoom=0.8)
 
 for i in range(number):
     imscatter(encoderR_train_value[i,0], encoderR_train_value[i,1], decoderR_train_value[i,:,:,0], ax=ax, zoom=0.8)
+
+
 '''
-
-
 imscatter(encoderR_fake_train_value[number,0], encoderR_fake_train_value[number,1], decoderR_fake_train_value[number,:,:,0], ax=ax, zoom=0.8)
 
 imscatter(encoderR_train_value[number,0], encoderR_train_value[number,1], decoderR_train_value[number,:,:,0], ax=ax, zoom=0.8)
+'''
 
 ax.scatter(x1, y1, s=5, c="red")
 ax.scatter(x2, y2, s=5, c="blue")
