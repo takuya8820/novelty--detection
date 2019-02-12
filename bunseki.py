31e8ac676a05c68c7b39a56d701a4220caf43fbb
takuya8820 <s206226@center.wakayama-u.ac.jp>
2019-02-12 10:42:32 +0900

    可視化

--- bunseki.py
+++ bunseki.py
@@ -94,7 +94,6 @@ plt.gray()
 
 for i in range(number):
     imscatter(encoderR_fake_train_value[i,0], encoderR_fake_train_value[i,1], decoderR_fake_train_value[i,:,:,0], ax=ax, zoom=0.8)
-
 for i in range(number):
     imscatter(encoderR_train_value[i,0], encoderR_train_value[i,1], decoderR_train_value[i,:,:,0], ax=ax, zoom=0.8)
 
